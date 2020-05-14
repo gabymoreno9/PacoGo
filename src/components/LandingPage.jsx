cc18de34f43f871eb443cc593fcb9df1f9e0dc3f
Gabriela Moreno <gabrielaxmoreno@gmail.com>
2020-05-13 22:40:54 -0700

    landing page with game play button done

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import {withRouter} from 'react-router-dom';
+
 
 import paco from '../assets/paco.png'
 
@@ -6,8 +9,12 @@ import paco from '../assets/paco.png'
 class LandingPage extends React.Component {
   render = () =>
     <div className="landing-page">
-      <img src={paco} style={{ width: '280px', height: '300px' }} alt="logo" />
+        <img src={paco} style={{ width: '280px', height: '300px' }} alt="logo" />
+        <Button 
+        variant="contained" style={{marginTop: '20px'}}onClick={() => this.props.history.push("play")}>Play
+        </Button>
     </div>
+    
 }
 
-export default LandingPage
+export default withRouter(LandingPage)
